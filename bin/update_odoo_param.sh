5e2a95ca60ea0432f85e3033bdebc031d4e577e4
Sébastien Maillard <sebastien.maillard@elico-corp.com>
2017-08-18 20:52:51 +0800

    Completely change the odoo.conf parameter substitution
    Before, it was based on a static list of parameters to be
    substituted. Now, any parameter can be substituted. The pattern
    for the parameters is "ODOO_<name of the parameter>" (big caps).

---
+++ bin/update_odoo_param.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# FIXME the below code should be in boot but for an unknown reason, the
+# instruction `grep -q "^$param\s*=" $odoo_conf_file` fails to run in boot, when it
+# works fine here.
+# Both are bash scripts, but:
+#  - boot is run by Docker as an entrypoint
+#  - target_user.sh is run using bash command
+#
+log_src='['${0##*/}']'
+
+odoo_user=$1
+odoo_conf_file=$2
+param=$3
+val=$4
+
+# Check if the conf already contains that parameter
+grep -q "^$param\s*=" $odoo_conf_file
+
+if [ $? -eq 0 ]; then
+    # Substitute the value
+    sudo -i -u $odoo_user sed -i "s/^$param\s*=.*/$param = $val/g" $odoo_conf_file
+else
+    # Append the parameter (hide tee output to stdout)
+    echo "$param = $val" | sudo -i -u $odoo_user tee -a $odoo_conf_file > /dev/null
+fi

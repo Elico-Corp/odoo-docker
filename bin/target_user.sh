269cdaae892d25811195f51e8767d1d3933e719f
Sébastien Maillard <sebastien.maillard@elico-corp.com>
2017-08-18 16:51:48 +0800

    Simplify/refactor target Odoo user mapping

--- bin/target_user.sh
+++ bin/target_user.sh
@@ -8,26 +8,33 @@
 #
 log_src='['${0##*/}']'
 
+odoo_user='odoo'
+
 # Check if there's a target user to run Odoo
 if [ "$TARGET_ID" ]; then
     echo $log_src[`date +%F.%H:%M:%S`]' Check if target user exists...'
-    EXISTS=$( getent passwd $TARGET_ID | wc -l )
+    exists=$( getent passwd $TARGET_ID | wc -l )
 
     # Create target user
-    if [ $EXISTS == "0" ]; then
-        echo $log_src[`date +%F.%H:%M:%S`]' Create target user...'
-        adduser --uid $TARGET_ID --disabled-password --gecos "" --shell=/bin/bash target-user
+    if [ $exists == "0" ]; then
+        echo $log_src[`date +%F.%H:%M:%S`]' Create target Odoo user...'
+        odoo_user='target-odoo-user'
+        adduser --uid $TARGET_ID --disabled-password --gecos "" --shell=/bin/bash $odoo_user
 
         # Add target user to odoo group so that he can read/write the content of /opt/odoo
         echo $log_src[`date +%F.%H:%M:%S`]' Add target user to odoo group...'
-        usermod -a -G odoo target-user
-    # If the user already exists, check if it's the same as odoo
+        usermod -a -G odoo $odoo_user
     else
         echo $log_src[`date +%F.%H:%M:%S`]' Target user already exists, make sure it is odoo...'
-        ODOO_ID=$( id -u odoo )
-        if [ $TARGET_ID -ne $ODOO_ID ]; then
+        odoo_user_id=$( id -u $odoo_user )
+
+        # If the user already exists, check if it's the same as odoo
+        if [ $TARGET_ID -ne $odoo_user_id ]; then
             echo $log_src[`date +%F.%H:%M:%S`]' The target user is not the same as odoo, exiting'
             exit 1
         fi
     fi
 fi
+
+# Return target Odoo user to boot script
+echo $odoo_user > /tmp/odoo_user

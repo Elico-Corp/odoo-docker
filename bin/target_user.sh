e62270989a9cf67421ac9102b9bee4e4e595fa97
Sébastien Maillard <sebastien.maillard@elico-corp.com>
2017-08-21 15:12:55 +0800

    Make log less verbose

--- bin/target_user.sh
+++ bin/target_user.sh
@@ -12,20 +12,20 @@ odoo_user='odoo'
 
 # Check if there's a target user to run Odoo
 if [ "$TARGET_ID" ]; then
-    echo $log_src[`date +%F.%H:%M:%S`]' Check if target user exists...'
+    # Check whether target user exists or not
     exists=$( getent passwd $TARGET_ID | wc -l )
 
     # Create target user
     if [ $exists == "0" ]; then
-        echo $log_src[`date +%F.%H:%M:%S`]' Create target Odoo user...'
+        echo $log_src[`date +%F.%H:%M:%S`]' Creating target Odoo user...'
         odoo_user='target-odoo-user'
         adduser --uid $TARGET_ID --disabled-password --gecos "" --shell=/bin/bash $odoo_user
 
         # Add target user to odoo group so that he can read/write the content of /opt/odoo
-        echo $log_src[`date +%F.%H:%M:%S`]' Add target user to odoo group...'
+        echo $log_src[`date +%F.%H:%M:%S`]' Adding user to `odoo` group...'
         usermod -a -G odoo $odoo_user
     else
-        echo $log_src[`date +%F.%H:%M:%S`]' Target user already exists, make sure it is odoo...'
+        # Target user already exists, make sure it's odoo
         odoo_user_id=$( id -u $odoo_user )
 
         # If the user already exists, check if it's the same as odoo
